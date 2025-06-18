761e98c96376ce91dd74ed25c17b9c571b867eb1
Lucas Bighi <lucasmbighi@icloud.com>
2025-06-18 16:52:37 -0300

    Initial Commit

---
+++ SwiftyChat/SwiftyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyChatApp.swift
+//  SwiftyChat
+//
+//  Created by Lucas Bighi on 18/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftyChatTests/SwiftyChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyChatTests.swift
+//  SwiftyChatTests
+//
+//  Created by Lucas Bighi on 18/06/25.
+//
+
+import Testing
+@testable import SwiftyChat
+
+struct SwiftyChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
